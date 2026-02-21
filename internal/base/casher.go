c505d5333975a199f431724a495be24a3f01777e
dejitarudemon <fine.usr1@yandex.ru>
2026-02-21 17:40:42 +0300

    Добавлено объявление интерфейса для L1-кеша

---
+++ internal/base/casher.go
@@ -0,0 +1,51 @@
+package base
+
+import "context"
+
+/*
+This file contains interface for engine cashing
+*/
+
+/*
+Interface Casher is intended for L1-cash to avoid re-searching sruct fields
+by reflect package. It must be thread-safe
+*/
+type Casher interface {
+	/*
+		Set function is used to set some value under a key in L1-cash
+
+		Parameters:
+			- ctx - perfoming context
+			- key - any string value providing to value
+			- value - struct, primitve or custom type etc
+
+		Returns:
+			- error - set value error.
+	*/
+	Set(ctx context.Context, key string, value any) error
+
+	/*
+		Get function is used to get some value from L1-cash by a key
+
+		Parameters:
+			- ctx - perfoming context
+			- key - any string value providing to value
+
+		Returns:
+			- any - storaged value if it's exist. If error occured or it's not found, the function must return Nil
+			- error - get value error. If value is not found, must be Nil.
+	*/
+	Get(ctx context.Context, key string) (any, error)
+
+	/*
+		Clyear function is used to clear value by its key
+
+		Parameters:
+			- ctx - perfoming context
+			- key - any string value providing to value
+
+		Returns:
+			- error - clear error. If value is not found, must be Nil.
+	*/
+	Clear(ctx context.Context, key string) error
+}
